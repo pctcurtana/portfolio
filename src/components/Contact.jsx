91852170ab2fd7b2e7a790b5f4218af61c8902ba
thatx2 <phamthat2206@gmail.com>
2025-06-06 00:54:45 +0700

    fix UI contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,12 @@ export default function () {
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <div className="flex items-center gap-2 w-xl h-10 border-x border-t mt-4 border-white/50 rounded-t-xl">
+            <div className="flex items-center gap-2 w-sm sm:w-2xl h-10 border-x border-t mt-4 border-white/50 rounded-t-xl">
                 <p className="h-4 w-4 bg-red-500 rounded-full ml-2"></p>
                 <p className="h-4 w-4 bg-amber-400 rounded-full"></p>
                 <p className="h-4 w-4 bg-green-500 rounded-full"></p>
             </div>
-            <div className="w-xl h-100 border border-white/50 rounded-b-xl p-4 text-sm  font-fira-code">
+            <div className="w-sm sm:w-2xl h-100 border border-white/50 rounded-b-xl p-4 text-sm  font-fira-code">
                 <p className="text-white"> {'>'} npm install package ...</p>
                 <p className="text-green-500">contact through my facebook </p>
            </div>
