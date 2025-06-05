7f068f5536d55baf111a1f6a0c0fd8debd3723d7
thatx2 <phamthat2206@gmail.com>
2025-06-06 00:42:14 +0700

    add UI contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/fira-code/400.css'
+import '@fontsource/fira-code/500.css'
+import '@fontsource/fira-code/600.css'
+import '@fontsource/fira-code/700.css'
 import './index.css'
 import App from './App.jsx'
 
